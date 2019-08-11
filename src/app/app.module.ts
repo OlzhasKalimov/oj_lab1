2c8aee1ff101de13398959415d9fd34971e1776d
OlzhasKalimov <olzhas.kalimov@yahoo.com>
2019-08-11 18:52:42 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutComponent,
+        ContactComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        NgbModule.forRoot(),
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        ComponentsModule,
+        ExamplesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { LoginComponent } from './examples/login/login.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+    { path: 'index',                component: ComponentsComponent },
+    { path: 'nucleoicons',          component: NucleoiconsComponent },
+    { path: 'examples/landing',     component: LandingComponent },
+    { path: 'examples/login',       component: LoginComponent },
+    { path: 'examples/profile',     component: ProfileComponent }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
